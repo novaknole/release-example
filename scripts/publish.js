a76ebb0219ebc5ff6c76dfde45e020cb3979fb12
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-05 22:15:17 +0400

    again

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,5 +1,5 @@
 const {publish} = require("@changesets/release-utils");
 
 module.exports = ({ github, context }) => {
-    publish({script: 'changeset publish'})
+    publish({script: 'yarn release'})
 }
