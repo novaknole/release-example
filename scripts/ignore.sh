7dbc03b7088a2ba28af59bb0f4164ae9d48036a7
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-05 15:10:03 +0400

    kk

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -12,6 +12,6 @@ arr=("@glagh/giorgi-contracts-monorepo" "@glagh/giorgi-configs-monorepo")
 # Loop through the array
 for item in "${arr[@]}"; do
   if [[ "$item" != "$PACKAGE_NAME_TO_PUBLISH" ]]; then
-    changeset version --ignore @item
+    changeset version --ignore $item
   fi
 done
