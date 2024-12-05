9dff09cc44ccb047a21c4b445bcc76dba803e5f3
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-05 15:00:08 +0400

    again

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -2,6 +2,16 @@
 
 set -euo pipefail
 
-echo "dajsdajsdj"
+echo "awesome123"
 
 echo $PACKAGE_NAME_TO_PUBLISH
+
+arr=("@glagh/giorgi-contracts-monorepo", "@glagh/giorgi-configs-monorepo")
+
+
+# Loop through the array
+for item in "${arr[@]}"; do
+  if [[ "$item" != "$PACKAGE_NAME_TO_PUBLISH" ]]; then
+    echo "$item"
+  fi
+done
