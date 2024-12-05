00b23556bc32b915c9a26b18deac97ffe4005aa8
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-05 16:08:18 +0400

    again

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -2,28 +2,37 @@
 
 set -euo pipefail
 
-RELEASE_BRANCH=changeset-release/main
+if [ "$WHEN" == "before" ]; then
+    TEMP_BRANCH=changeset-temp-release/main
+    RELEASE_BRANCH=changeset-release/main
 
-echo "awesome123"
+    echo "awesome123"
 
-echo $PACKAGE_NAME_TO_PUBLISH
+    echo $PACKAGE_NAME_TO_PUBLISH
 
-arr=("@glagh/giorgi-contracts-monorepo" "@glagh/giorgi-configs-monorepo")
+    arr=("@glagh/giorgi-contracts-monorepo" "@glagh/giorgi-configs-monorepo")
 
-git checkout -b "$RELEASE_BRANCH"
+    git checkout -b "$TEMP_BRANCH"
 
-# Loop through the array
-for item in "${arr[@]}"; do
-  if [[ "$item" != "$PACKAGE_NAME_TO_PUBLISH" ]]; then
-    changeset version --ignore $item
-  fi
-done
+    # Loop through the array
+    for item in "${arr[@]}"; do
+    if [[ "$item" != "$PACKAGE_NAME_TO_PUBLISH" ]]; then
+        changeset version --ignore $item
+    fi
+    done
 
-git add .
-git commit -m "Ignored changesets of all packages except $PACKAGE_NAME_TO_PUBLISH"
+    git add .
+    git commit -m "Ignored changesets of all packages except $PACKAGE_NAME_TO_PUBLISH"
 
-# Push branch
-if ! git push origin "$RELEASE_BRANCH"; then
-  echo "Can't push $RELEASE_BRANCH."
-  exit 1
+    # Push branch
+    if ! git push origin "$TEMP_BRANCH"; then
+        echo "Can't push $TEMP_BRANCH."
+        exit 1
+    fi
+fi
+
+if [ "$WHEN" == "after" ]; then
+    git checkout $RELEASE_BRANCH
+    git merge $TEMP_BRANCH
+    git branch -D $TEMP_BRANCH
 fi
