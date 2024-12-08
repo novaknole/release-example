07b39f8d0c7790c3dd154b93a96d42adf503f82b
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-08 23:48:25 +0400

    oe

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -5,21 +5,29 @@ set -euo pipefail
 # Define the mapping as an associative array
 mapping="contracts:@glagh/giorgi-contracts-monorepo configs:@glagh/giorgi-configs-monorepo"
 
-echo $REF_NAME
+branch = "${REF_NAME##*-}"
 
 # if we did workflow dispatch manually on release-v, that means we want to release main version, so we exit the pre mode.
 # if something was pushed to release-.., we still don't exit pre-mode.
 
 echo $EVENT_NAME
+echo $REF_NAME
+echo $branch
 
-echo "Loopsing through keyss and values:"
+## If the below is true, it means, we're doing the final version release(without rc)
+## So we exit and re-run the changeset version.
+if [[ "$EVENT_NAME" == "workflow_dispatch" ]]; then
+    npx changeset pre exit alpha
+fi
+
+echo "Looping through keys and values:"
 
 # for pair in $mapping; do
-# key="${pair%%:*}"
-# value="${pair#*:}"
+key="${pair%%:*}"
+value="${pair#*:}"
 
-#     if [[ "$key" != "$package" ]]; then
-#         changeset version --ignore $value
-#     fi
-# done
+    if [[ "$key" != "$package" ]]; then
+        changeset version --ignore $value
+    fi
+done
 
