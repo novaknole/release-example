336fce6fb7cb069be55d661c9283b389291e4266
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-08 23:08:03 +0400

    again

--- scripts/ignore.sh
+++ scripts/ignore.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Define the mapping as an associative array
 mapping="contracts:@glagh/giorgi-contracts-monorepo configs:@glagh/giorgi-configs-monorepo"
 
-echo $PACKAGE
+echo $REF_NAME
 
 # if we did workflow dispatch manually on release-v, that means we want to release main version, so we exit the pre mode.
 # if something was pushed to release-.., we still don't exit pre-mode.
