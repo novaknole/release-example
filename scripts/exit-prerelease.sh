418ec799b1faae5f6c66ff77ef13a0efcddef496
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-09 10:05:17 +0400

    ss

--- scripts/exit-prerelease.sh
+++ scripts/exit-prerelease.sh
@@ -7,6 +7,13 @@ git add .
 git commit -m "Exit release candidate"
 git push origin
 
+echo "arvici"
+echo $REF_NAME
+
 git checkout changeset-release/$REF_NAME
 
+echo "awesome"
+
 git merge $REF_NAME
+
+echo "good"
