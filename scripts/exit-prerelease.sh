3143c91596d9c5efb6b0daba68afb5c36487ad79
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-09 10:23:22 +0400

    again

--- scripts/exit-prerelease.sh
+++ scripts/exit-prerelease.sh
@@ -6,16 +6,3 @@ npx changeset pre exit alpha
 git add .
 git commit -m "Exit release candidate"
 git push origin
-
-echo "arvici"
-echo $REF_NAME
-
-git checkout changeset-release/$REF_NAME
-
-echo "awesome"
-
-git merge $REF_NAME
-
-git checkout $REF_NAME
-
-echo "good"
